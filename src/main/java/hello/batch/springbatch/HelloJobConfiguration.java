0761f77d3bc91671eff5b6c6da33bd1ff38a06e3
yoonmin-kim <rladbsals23@gmail.com>
2022-01-25 22:47:48 +0900

    docs: StepBuilderFactory/ StepBuilder
    StepBuilderFactory:
    - StepBuilder 를 생성하는 클래스이며 get("stepName") 메서드를 제공한다
    
    StepBuilder:
    - Step 을 구성하는 설정에 따라 하위 다섯개의 StepBuilder 를 생성하고 실제 Step 생성을 위임한다
    - TaskletStepBuilder
      · TaskletStep 을 생성하는 기본 빌더 클래스
    - SimpleStepBuilder
      · TaskletStep 을 생성하지만 내부에는 chunk 기반 작업을 처리하는 ChunkOrientedTasklet 클래스를 생성한다
    - PartitionStepBuildr
      · PartitionStep 을 생성하며 멀티스레드 방식으로 Job 을 실행한다
    - JobStepBuilder
      · JobStep 을 생성하며 Step 안에서 Job 을 실행한다
    - FlowStepBuilder
      · FlowStep 을 생성하며 Step 안에서 Flow 를 실행한다

--- src/main/java/hello/batch/springbatch/HelloJobConfiguration.java
+++ src/main/java/hello/batch/springbatch/HelloJobConfiguration.java
@@ -1,5 +1,7 @@
 package hello.batch.springbatch;
 
+import java.util.List;
+
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobExecutionListener;
@@ -14,9 +16,18 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.job.DefaultJobParametersValidator;
 import org.springframework.batch.core.job.builder.FlowBuilder;
 import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.NonTransientResourceException;
+import org.springframework.batch.item.ParseException;
+import org.springframework.batch.item.UnexpectedInputException;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.batch.repeat.policy.DefaultResultCompletionPolicy;
+import org.springframework.batch.repeat.policy.SimpleCompletionPolicy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -33,12 +44,21 @@ public class HelloJobConfiguration {
 	@Bean
 	public Job helloJob() {
 		return jobBuilderFactory.get("helloJob")
+			.incrementer(new RunIdIncrementer())
 			.start(helloStep1())
 			.next(helloStep2())
+			.next(helloStep3())
+			.next(helloStep4())
 			// .validator(new MyJobValidator())
 			// .validator(new DefaultJobParametersValidator(new String[]{"name"},new String[]{"count"}))
-			.incrementer(new MyJobParametersIncrementer())
 			// .preventRestart()
+			.build();
+	}
+	@Bean
+	public Job job() {
+		return jobBuilderFactory.get("job")
+			.start(helloStep1())
+			.next(helloStep2())
 			.build();
 	}
 
@@ -60,14 +80,43 @@ public class HelloJobConfiguration {
 	@Bean
 	public Step helloStep2() {
 		return stepBuilderFactory.get("helloStep2")
-			.tasklet((stepContribution, chunkContext) -> null)
+			.<String, String>chunk(3)
+			.reader(new ItemReader<String>() {
+				@Override
+				public String read() throws
+					Exception,
+					UnexpectedInputException,
+					ParseException,
+					NonTransientResourceException {
+					return null;
+				}
+			})
+			.processor(new ItemProcessor<String, String>() {
+				@Override
+				public String process(String s) throws Exception {
+					return null;
+				}
+			})
+			.writer(new ItemWriter<String>() {
+				@Override
+				public void write(List<? extends String> list) throws Exception {
+
+				}
+			})
 			.build();
 	}
 
 	@Bean
 	public Step helloStep3() {
 		return stepBuilderFactory.get("helloStep3")
-			.tasklet((stepContribution, chunkContext) -> null)
+			.partitioner(helloStep1())
+			.build();
+	}
+
+	@Bean
+	public Step helloStep4() {
+		return stepBuilderFactory.get("helloStep4")
+			.job(job())
 			.build();
 	}
 
