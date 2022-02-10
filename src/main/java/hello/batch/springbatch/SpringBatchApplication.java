01b863a9d0d3e164f3919422c05020cc8496a40c
yoonmin-kim <rladbsals23@gmail.com>
2022-02-10 23:46:53 +0900

    docs: @JobScope/@StepScope - 기본개념 및 설정
    기본개념:
    - Job 과 Step 의 빈 생성과 실행에 관여하는 스코프
    - 프록시 모드를 기본값으로 하는 스코프 - @Scope(value="job", proxyMode=ScopedProxyMode.TARGET_CLASS)
    - 해당 스코프가 선언되면 빈의 생성이 어플리케이션 구동시점이 아닌 실행시점에 이루어진다
      · @Values 를 주입해서 빈의 실행 시점에 값을 참조할 수 있으며 일종의 Lazy Binding 이 가능해 진다
      · @Value("#{jobParameters[파라미터명]}"), @Value("#{jobExecutionContext[파라미터명]}")
      @Value("#{stepExecutionContext[파라미터명]}")
      · @Values 를 사용할 경우 빈 선언문에 @JobScope, @StepScope 를 정의하지 않으면 오류를 발생하므로 반드시 선언해야 함
    - 프록시 모드로 빈이 선언되기 때문에 어플리케이션 구동시점에는 빈의 프록시 객체가 생성되어 실행 시점에 실제 빈을 호출해 준다
    - 병렬처리 시 각 스레드 마다 생성된 스코프 빈이 할당되기 때문에 스레드에 안전하게 실행이 가능하다
    
    @JobScope
    - Step 선언문에 정의한다
    - @Value : jobParameters, jobExecutionContext 만 사용가능
    
    @StepScope
    - Tasklet 이나 ItemReader, ItemWriter, ItemProcessor 선언문에 정의한다
    - @Value : jobParameters, jobExecutionContext, stepExecutionContext 사용가능

--- src/main/java/hello/batch/springbatch/SpringBatchApplication.java
+++ src/main/java/hello/batch/springbatch/SpringBatchApplication.java
@@ -3,9 +3,11 @@ package hello.batch.springbatch;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableBatchProcessing
+@ComponentScan(basePackages = {"hello.batch.springbatch.scope"})
 public class SpringBatchApplication {
 
 	public static void main(String[] args) {
