763542730197c998180341cf819bd40de83bfd20
yoonmin-kim <rladbsals23@gmail.com>
2022-02-12 21:11:43 +0900

    docs: Chunk
    기본개념:
    - Chunk 란 여러 개의 아이템을 묶은 하나의 덩어리, 블록을 의미
    - 한번에 하나씩 아이템을 입력 받아 Chunk 단위의 덩어리로 만든 후 Chunk 단위로 트랜잭션을 처리함,
    즉 Chunk 단위의 Commit 과 Rollback 이 이루어짐
    - 일반적으로 대용량 데이터를 한번에 처리하는 것이 아닌 청크 단위로 쪼개어서 더 이상 처리할 데이터가
    없을때 까지 반복해서 입출력하는데 사용됨
    
    Chunk<I> vs Chunk<O>
    - Chunk<I> 는 ItemReader 로 읽은 하나의 아이템을 Chunk 에서 정한 개수만큼 반복해서 저장하는 타입
    - Chunk<O> 는 ItemReadr 로부터 전달받은 Chunk<I> 를 참조해서 ItemProcessor 에서 적절하게 가공,
    필터링한 다음 ItemWriter 에 전달하는 타입

---
+++ src/main/java/hello/batch/springbatch/chunk/ChunkConfiguration.java
@@ -0,0 +1,66 @@
+package hello.batch.springbatch.chunk;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Configuration
+public class ChunkConfiguration {
+
+	private final JobBuilderFactory jobBuilderFactory;
+	private final StepBuilderFactory stepBuilderFactory;
+
+	@Bean
+	public Job chunkJob() {
+		return jobBuilderFactory.get("chunkJob")
+			.start(step1())
+			.next(step2())
+			.build();
+	}
+
+	@Bean
+	public Step step1() {
+		return stepBuilderFactory.get("step1")
+			.<String, String>chunk(3)
+			.reader(new ListItemReader<>(Arrays.asList("item1", "item2", "item3")))
+			.processor(new ItemProcessor<String, String>() {
+				@Override
+				public String process(String s) throws Exception {
+					System.out.println("s = " + s);
+					return "my"+s;
+				}
+			})
+			.writer(new ItemWriter<String>() {
+				@Override
+				public void write(List<? extends String> list) throws Exception {
+					System.out.println("list = " + list);
+				}
+			})
+			.build();
+	}
+
+	@Bean
+	public Step step2() {
+		return stepBuilderFactory.get("step2")
+			.tasklet((stepContribution, chunkContext) -> RepeatStatus.FINISHED)
+			.build();
+
+	}
+}
