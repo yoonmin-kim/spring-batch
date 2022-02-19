4f03d1304da60a5027fe5104c108986c7bd4878d
yoonmin-kim <rladbsals23@gmail.com>
2022-02-19 18:39:42 +0900

    feat: FlatFileItemReader - fixedLengthTokenizer
    기본개념:
    - 한 개 라인의 String 을 사용자가 설정한 고정길이 기준으로 나누어 토큰화 하는 방식
    - 범위는 문자열 형식으로 설정 할 수 있다
      · "1-4" 또는 "1-3,4-6,7" 또는 "1-2,4-5,7-10"
      · 마지막 범위가 열려 있으면 해당 위치부터 끝까지 읽는다

---
+++ src/main/java/hello/batch/springbatch/flatfile/fixedlengthtokenizer/FixedLengthTokenizerConfiguration.java
@@ -0,0 +1,78 @@
+package hello.batch.springbatch.flatfile.fixedlengthtokenizer;
+
+import java.util.List;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.transform.FixedLengthTokenizer;
+import org.springframework.batch.item.file.transform.Range;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+
+import hello.batch.springbatch.flatfile.Client;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Configuration
+public class FixedLengthTokenizerConfiguration {
+
+	private final JobBuilderFactory jobBuilderFactory;
+	private final StepBuilderFactory stepBuilderFactory;
+
+	@Bean
+	public Job fixedLengthTokenizerJob() {
+		return jobBuilderFactory.get("fltJob")
+			.start(step1())
+			.next(step2())
+			.build();
+	}
+
+	@Bean
+	public Step step1() {
+		return stepBuilderFactory.get("step1")
+			.chunk(5)
+			.reader(itemReader())
+			.writer(new ItemWriter() {
+				@Override
+				public void write(List items) throws Exception {
+					System.out.println("items = " + items);
+				}
+			})
+			.build();
+	}
+
+	@Bean
+				public ItemReader<?> itemReader() {
+		return new FlatFileItemReaderBuilder<Client>()
+			.name("flatfile")
+			.targetType(Client.class)
+			.resource(new FileSystemResource("C:\\study\\spring-batch\\src\\main\\resources\\client.txt"))
+			.linesToSkip(1)
+			.fixedLength()
+			.addColumns(new Range(1,5))
+			.addColumns(new Range(6,9))
+			.addColumns(new Range(10,11))
+			.names("name", "year", "age")
+			.build()
+			;
+	}
+
+	@Bean
+	public Step step2() {
+		return stepBuilderFactory.get("step2")
+			.tasklet((stepContribution, chunkContext) -> RepeatStatus.FINISHED)
+			.build();
+	}
+}
