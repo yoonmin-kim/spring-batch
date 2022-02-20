26ac598fbc64997e2f2d2d9049c5dedf5c226ee3
yoonmin-kim <rladbsals23@gmail.com>
2022-02-20 13:38:56 +0900

    docs: FlatFileItemReader - Exception Handling
    기본개념:
    - 라인을 읽거나 토큰화 할 때 발생하는 Parsing 예외를 처리할 수 있도록 예외 계층 제공
    - 토큰화 검증을 엄격하게 적용하지 않도록 설정하면 Parsing 예외가 발생하지 않도록 할 수 있다
    
    IncorrectTokenCountException:
    - extends FlatFileFormatException
    - DelimitedLineTokenizer 로 토큰화 할 때 컬럼 개수와 실제 토큰화 한 컬럼 수와 다를 때
    발생하는 예외
    
    IncorrectLineLengthException:
    - extends FlatFileFormatException
    - FixedLengthLineTokenizer 로 토큰화 할 때 라인 전체 길이와 컬럼 길이의 총합과 일치하지
    않을 때 발생

--- src/main/java/hello/batch/springbatch/flatfile/fixedlengthtokenizer/FixedLengthTokenizerConfiguration.java
+++ src/main/java/hello/batch/springbatch/flatfile/fixedlengthtokenizer/FixedLengthTokenizerConfiguration.java
@@ -4,17 +4,11 @@ import java.util.List;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.file.LineMapper;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
-import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
-import org.springframework.batch.item.file.transform.FixedLengthTokenizer;
 import org.springframework.batch.item.file.transform.Range;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
